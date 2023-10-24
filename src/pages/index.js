419a1358b94f602f34d1abda84cb85619fab6adc
a-elnawawy <eng.ahmed.elnawawy@gmail.com>
2023-10-25 01:34:01 +0300

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
 const Home = () => {
   return (
-    <main>
-      <p>Hi,</p>
-
-      <p>You are in the project</p>
+    <main
+      style={{
+        display: 'grid',
+        height: '100vh',
+        justifyContent: 'center',
+        alignContent: 'center',
+        gap: '20px',
+      }}
+    >
+      <p>Hi, You are in the</p>
 
       <h1>Main Page</h1>
+
+      <p>Please I You Are A FE, Remove All This JSX Code Before You Start</p>
     </main>
   );
 };
