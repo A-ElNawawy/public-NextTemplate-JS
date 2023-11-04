e38d48f0c370b795e784a9935439625dce20ef28
a-elnawawy <eng.ahmed.elnawawy@gmail.com>
2023-11-04 19:14:33 +0200

    - add the new font method to the NextJSTemplate

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,16 @@
+import { IBMPlexSansArabic } from 'src/fonts';
+
 import 'src/globals.css';
 
+const dir = 'rtl';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div
+      className={IBMPlexSansArabic.className}
+      dir={dir}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 }
